96210bb1d2383bcabed233f0c4c08b0879e0a584
thobio <thobio.j@zerone-consulting.com>
2020-05-08 12:30:32 +0530

    Initial Commit

---
+++ Twitter IOS/Controllers/MainTabController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabController.swift
+//  Twitter IOS
+//
+//  Created by Zerone on 08/05/20.
+//  Copyright © 2020 Coding Crackers. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
